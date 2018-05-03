b8904e684ea550ed51804aa53fa112bc7e63ca4d
Olya Tilichenko <olga@yurys-macbook-pro.local>
2018-05-03 16:38:00 +0300

    Initial Commit

---
+++ SearchPlacesYelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchPlacesYelp
+//
+//  Created by Olga on 5/3/18.
+//  Copyright © 2018 Olga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
